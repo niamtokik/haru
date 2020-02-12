a13fde470291c19c5a2501138d77454ad1237c50
niamtokik <niamtokik@localhost>
2020-02-12 22:08:31 +0000

    small update

--- c_src/haru.c
+++ c_src/haru.c
@@ -3,19 +3,28 @@
 #include <string.h>
 #include "haru_comm.h"
 #include "hpdf.h"
+#define BUFFER 256
+#define FD_READ 0
+#define FD_WRITE 1
+
+void
+loop(int bufsize) {
+  unsigned char buffer[bufsize];
+  size_t length;
+
+  while((length=read_length2(FD_READ))<bufsize) {
+      read_data(FD_READ, buffer, length);
+      write_length2(FD_WRITE, length);
+      write_data(FD_WRITE, buffer, length);
+  }
+  
+  length = strlen("error");
+  write_length2(FD_WRITE, length);
+  write_data(FD_WRITE, "error", length);
+}
 
 int
-main(void) {
-  unsigned char buffer[256];
-  size_t length;
-  
-  while((length=read_length2(0))<255) {
-      read_data(0, buffer, length);
-      write_length2(1, length);
-      write_data(1, buffer, length);
-  }
-  length = strlen("error");
-  write_length2(1, length);
-  write_data(1, "error", length);
+main(int argc, char *argv[]) {
+  loop(BUFFER);
 }
 
