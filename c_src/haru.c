cafe0c9dbbb8d62d0d54edd3582e72b253d91205
niamtokik <niamtokik@localhost>
2020-02-12 20:26:43 +0000

    just a more 'complete' example

--- c_src/haru.c
+++ c_src/haru.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "haru_comm.h"
 #include "hpdf.h"
 
 int
 main(void) {
   unsigned char buffer[256];
-  while(1) {
-    size_t length = read_length2(0);
-    read_data(0, buffer, length);
-    
-    write_length2(1, length);
-    write_data(1, buffer, length);
+  size_t length;
+  
+  while((length=read_length2(0))<255) {
+      read_data(0, buffer, length);
+      write_length2(1, length);
+      write_data(1, buffer, length);
   }
+  length = strlen("error");
+  write_length2(1, length);
+  write_data(1, "error", length);
 }
 
